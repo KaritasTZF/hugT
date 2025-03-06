cd8caa0405e80a3c7a05da3f1a60f5b415340347
Karitas <ktz1@hi.is>
2025-03-06 01:07:30 +0000

    Init skeleton

---
+++ Trip.java
@@ -0,0 +1,34 @@
+package hugT;
+
+import java.util.ArrayList; //Til þess að geyma lista af object okkar
+
+public class Trip {
+    private String tripID;
+    private int people;
+    private int date;
+    private ArrayList<TripItem> items; //Listi af TripItems, listinn heitir items
+
+    //Constructor to initialize Trip object
+    public void Trip() {
+
+    }
+
+    //skilar fjöldi manns
+    public int getPeople() {
+        return this.people;
+    }
+
+    //Bætir við TripItem object
+    public void addTripItem(TripItem t) {
+        items.add(t);
+    }
+
+    //les verð öll TripItems, leggur saman og skilar heildarverð
+    public int getPrice() {
+        int verd = 0;
+        for (TripItem t: items) {
+            verd += t.getPrice();
+        }
+        return verd;
+    }
+}
---
+++ User.java
@@ -0,0 +1,15 @@
+package hugT;
+
+public class User {
+    private String userID;
+    private String name;
+    private String email;
+
+    //Constructor to initialize User object
+    public void User(String userID, String name, String email) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+    }
+
+}
