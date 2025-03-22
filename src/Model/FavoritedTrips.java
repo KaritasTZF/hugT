ea1eb8794fb6ee41bf45f6f17938ae9cd4b0086c
Loftur <lpe1@hi.is>
2025-03-22 17:03:24 +0000

    Bjó til fold-era, uppfærði model-inn og bjó til controller-ana okkar

---
+++ src/Model/FavoritedTrips.java
@@ -0,0 +1,33 @@
+package Model;
+
+import java.util.ArrayList; //Til þess að geyma lista af object okkar
+
+public class FavoritedTrips {
+    private int userID;
+    private ArrayList<Trip> trips;
+
+    // Constructor
+    public FavoritedTrips(int userID,ArrayList<Trip> trips) {
+        this.userID = userID;
+        this.trips = trips;
+
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public ArrayList<Trip> getTrips() {
+        return this.trips;
+    }
+
+    public void setTrips(ArrayList<Trip> trips) {
+        this.trips = trips;
+    }
+
+    // methods: add og remove trip.
+}
---
+++ src/Model/Flight.java
@@ -0,0 +1,93 @@
+package Model;
+
+import java.time.LocalDate;
+
+public class Flight {
+    // attributes hér
+    private String from;
+    private String to;
+    private int people;
+    private int days;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double price;
+    private String location;
+
+    // Constructor
+    public Flight(String from, String to, int people, int days, LocalDate startDate, LocalDate endDate, double price, String location) {
+        this.from = from;
+        this.to = to;
+        this.people = people;
+        this.days = days;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.price = price;
+        this.location = location;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public int getPeople() {
+        return this.people;
+    }
+
+    public void setPeople(int people) {
+        this.people = people;
+    }
+
+    public int getDays() {
+        return this.days;
+    }
+
+    public void setDays(int days) {
+        this.days = days;
+    }
+
+    public LocalDate getStartDate() {
+        return this.startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return this.endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    // methods hér. Aðallega getterar fyrir hvert attribute.
+}
