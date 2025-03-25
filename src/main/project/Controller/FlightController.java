38a90f41a0c154be87c69cbc92ac5b1cf6cc588b
Einar Geir Guðnason <egg30@hi.is>
2025-03-25 17:28:56 +0000

    breytingar á FlightController

--- src/main/project/Controller/FlightController.java
+++ src/main/project/Controller/FlightController.java
@@ -1,5 +1,25 @@
 package project.Controller;
 
+import project.Model.Flight;
+import java.util.Date;
+import java.util.List;
+
 public class FlightController {
 
+    private final SearchController searchController;
+
+    // fær SearchController til að leita að flugum
+    public FlightController(SearchController searchController) {
+        this.searchController = searchController;
+    }
+
+    //searchFlight(date, departure, arrival, people): List<Flight>
+    //Kallar á SearchController til að sækja tiltæk flug.
+
+    public List<Flight> searchFlight(Date date, String departure, String arrival, int people) {
+        // Kallar á SearchController-aðgerðina
+        List<Flight> flights = searchController.findAvailableFlights(date, departure, arrival, people);
+        // Fæ villu fyrir parametrana - Þarf að laga!
+        return flights;
+    }
 }
