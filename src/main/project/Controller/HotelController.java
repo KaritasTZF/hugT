99e5817693e0cd28e6d585dbc05c0ee1f364f20d
Einar Geir Guðnason <egg30@hi.is>
2025-03-25 16:52:39 +0000

    Breytingar á HotelController

--- src/main/project/Controller/HotelController.java
+++ src/main/project/Controller/HotelController.java
@@ -1,5 +1,20 @@
 package project.Controller;
+import project.Model.Hotel;
+import java.util.Date;
+import java.util.List;
 
 public class HotelController {
 
+    private final SearchController searchController;
+
+    public HotelController(SearchController searchController) {
+        this.searchController = searchController;
+    }
+
+    public List<Hotel> searchHotel(Date date, String location, int people) {
+        // Kallar á aðferðina í SearchController sem sækir hótel
+        List<Hotel> hotels = searchController.findAvailableHotels(date, location, people);
+        // Hér gætirðu gert frekari úrvinnslu ef þarf
+        return hotels;
+    }
 }
