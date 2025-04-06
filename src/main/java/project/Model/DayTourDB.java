387f35f2bcd2ee82440620b2510d03bbcd1d436a
Karitas <ktz1@hi.is>
2025-04-06 22:57:45 +0000

    laga 4D tengsl

--- src/main/java/project/Model/DayTourDB.java
+++ src/main/java/project/Model/DayTourDB.java
@@ -9,21 +9,24 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class DayTourDB {
-    private ArrayList<DayTour> dayTourList;
+    private final ArrayList<DayTour> dayTourList= new ArrayList<>();
 
     public DayTourDB() {
-    }
-    public void inialize() {
-        ObservableList<Tour> tours = TourDAO.listTours();
-        //Breytum 4D "Tour" objects í okkar DayTour objects. TourDAO tengist SQL.
-        for (Tour tour : tours) {
-            //TODO 4D hefur ekki fjöldi, og tími er geymdur sem String fyrir details
-            createDayTour(0, tour.getDate(), null, null, tour.getPrice(), tour.getCity());
+        //Fá öll 4D tours (þau eru ekki með filter)
+        try {
+            ObservableList<Tour> tours = TourDAO.listTours();
+            //Breytum 4D "Tour" objects í okkar DayTour objects. TourDAO tengist SQL.
+            for (Tour tour : tours) {
+                //TODO 4D hefur ekki fjöldi manns, og tími er geymdur sem String fyrir details
+                createDayTour(0, tour.getDate(), null, null, tour.getPrice(), tour.getCity());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void createDayTour(int people, LocalDate date, LocalDateTime timeStart, LocalDateTime timeEnd, int price, String location) {
-        dayTourList.add(new DayTour(people,date,timeStart,timeEnd,price,location));
+        this.dayTourList.add(new DayTour(people,date,timeStart,timeEnd,price,location));
     }
 
     public ArrayList<DayTour> getDayTourList(){
