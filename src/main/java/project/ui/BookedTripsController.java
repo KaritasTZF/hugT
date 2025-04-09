299d42b800b1ed16d43040b602dd1fd7d4491ab5
Karitas <ktz1@hi.is>
2025-04-09 01:01:44 +0000

    Búa til BookedTrips síðu

---
+++ src/main/java/project/ui/BookedTripsController.java
@@ -0,0 +1,27 @@
+package project.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class BookedTripsController {
+    @FXML private Button backButton;
+    @FXML private Label tripLabel;
+
+    //Back takki
+    public void goToWelcome() {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/project/ui/Welcome.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) backButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
